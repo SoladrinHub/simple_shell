4c5517258a3907fda1ce5c830b9f9420ae510769
Sear5566 <dwdsegun63@gmail.com>
2023-11-15 23:35:55 +0000

    this file checks for digits

---
+++ _isdigit.c
@@ -0,0 +1,16 @@
+#include "_shell.h"
+
+/**
+  *_isdigit - Check if a character is number
+  *@c: char to be check
+  *Return: 1 if letter, 0 if not
+  */
+int _isdigit(char c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
+
